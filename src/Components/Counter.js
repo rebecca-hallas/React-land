54e5be1f83984a23773c1d2e6805c855fc156728
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-14 23:19:44 +1200

    cleaning up code and css

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -4,13 +4,15 @@ function Counter() {
 const [count, setCount] = useState(1)
 
   return (
-    <div>
-        <h1>Counter</h1>
-        <h2>Fun with useState()</h2>
+    <>
+
+        <h1 className="text-4xl mt-10 mb-3 font-mono font-bold text-center">Counter</h1>
+       
+        <h2 className="text-lg mb-3 font-mono font-bold text-center">Fun with useState()</h2>
         <p>Welcome to the Counter component. This component has utilised the useState() function which is built-in to React. The initial state (count) is set to 1. The setCount() function returns the current count + 1, every time you click the Add button. Have fun clicking!</p>
         <p>State: {count}</p>
         <button onClick={() => setCount(count + 1)}>Click here to update the state</button>
-    </div>
+    </>
   );
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,13 +4,37 @@ import { Link } from 'react-router-dom'
 function Home () {
   return (
     <>
-    <h1 className="">React-land</h1>
-      <div>
+
+      <div className="flex justify-center">
+        <h1 className="text-4xl m-10 font-mono font-bold">Welcome to React-land</h1>
+      </div>
+
+      <div className="grid grid-cols-7 grid-rows-2">
+
+        <div className="col-start-2 row-start-2 col-span-1">
         <Link to="/Counter">
-          <button className="rounded-md bg-pink-400 border-2 border-white">
-            <h1>Counter</h1>
+          <button className="rounded-md bg-pink-400 hover:bg-pink-500 border-2 border-white p-3 text-white">
+            <h1 className="font-bold font-mono">Counter</h1>
             </button>
           </Link>
+        </div>
+
+      <div className="col-start-4 row-start-2 col-span-1">
+        <Link to="#">
+          <button className="rounded-md bg-pink-400 hover:bg-pink-500 border-2 border-white p-3 text-white">
+            <h1 className="font-bold font-mono">Button</h1>
+            </button>
+          </Link>
+        </div>
+
+        <div className="col-start-6 row-start-2 col-span-1">
+        <Link to="#">
+          <button className="rounded-md bg-pink-400 hover:bg-pink-500 border-2 border-white p-3 text-white">
+            <h1 className="font-bold font-mono">Button</h1>
+            </button>
+          </Link>
+        </div>
+
       </div>
     </>
   )
