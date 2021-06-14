3cc6549f9d6de5ea411a4c9d5a6628564843e156
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-14 19:32:54 +1200

    counter working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(1)
+
   return (
     <div className="App">
       <header className="App-header">
-   <h1>Hello</h1>
+        <h1>Hello</h1>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>Add</button>
       </header>
     </div>
   );
