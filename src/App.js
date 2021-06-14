f9ad4c3497a6de0e0e12bb074248f6ae2e59ba00
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-14 20:10:22 +1200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import './App.css';
-import React, { useState } from 'react';
+import './Styles.css';
+import Counter from './Counter'
 
 function App() {
-const [count, setCount] = useState(1)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Counter</h1>
-        <h3>Welcome to the App component. This component has utilised the useState() function which is built-in to React. The initial state (count) is set to 1. The setCount() function returns the current count + 1, every time you click the Add button. Have fun clicking!</h3>
-        <p>{count}</p>
-        <button onClick={() => setCount(count + 1)}>Add</button>
-      </header>
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 
---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Styles.css';
+
+function Counter() {
+const [count, setCount] = useState(1)
+
+  return (
+    <div className="container">
+        <h1>Counter</h1>
+        <h2>Fun with useState()</h2>
+        <p>Welcome to the App component. This component has utilised the useState() function which is built-in to React. The initial state (count) is set to 1. The setCount() function returns the current count + 1, every time you click the Add button. Have fun clicking!</p>
+        <p>State: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Click here to update the state</button>
+    </div>
+  );
+}
+
+export default Counter;
