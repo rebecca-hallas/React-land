27dd1be89b9d9f86b54fe294914d80a63d5343bf
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-14 23:05:55 +1200

    tailwind connected started styling

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -7,7 +7,7 @@ const [count, setCount] = useState(1)
     <div>
         <h1>Counter</h1>
         <h2>Fun with useState()</h2>
-        <p>Welcome to the App component. This component has utilised the useState() function which is built-in to React. The initial state (count) is set to 1. The setCount() function returns the current count + 1, every time you click the Add button. Have fun clicking!</p>
+        <p>Welcome to the Counter component. This component has utilised the useState() function which is built-in to React. The initial state (count) is set to 1. The setCount() function returns the current count + 1, every time you click the Add button. Have fun clicking!</p>
         <p>State: {count}</p>
         <button onClick={() => setCount(count + 1)}>Click here to update the state</button>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 function Home () {
   return (
     <>
+    <h1 className="">React-land</h1>
       <div>
         <Link to="/Counter">
-          <button>
+          <button className="rounded-md bg-pink-400 border-2 border-white">
             <h1>Counter</h1>
             </button>
           </Link>
